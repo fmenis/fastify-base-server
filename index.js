a850abdd3f52c68a49b7ff0466796df0c960392b
Filippo Menis <filippomeniswork@gmail.com>
2022-07-30 16:23:26 +0200

    added graceful shutdown

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import Fastify from 'fastify'
+import closeWithGrace from 'close-with-grace'
 
 import { buildServerOptions } from './src/utils/buildServeOptions.js'
 import App from './src/app.js'
@@ -7,6 +8,15 @@ const fastify = Fastify(buildServerOptions())
 
 await fastify.register(App)
 await fastify.ready()
+
+closeWithGrace({ delay: 500 }, async ({ signal, err }) => {
+  const { log } = fastify
+  if (err) {
+    log.error(err)
+  }
+  log.debug(`'${signal}' signal receiced. Gracefully closing fastify server`)
+  await fastify.close()
+})
 
 fastify.listen(
   {
